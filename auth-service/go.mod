f59bc02112ddb7a93aac324330eda081e10ad7a3
Almaz Galimov <almazwork@gmail.com>
2025-08-22 17:55:58 +0500

    add migrations

--- auth-service/go.mod
+++ auth-service/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/SoulStalker/EdReports/protos v0.0.0-20250822080945-444ec3d28d2d
 	github.com/fatih/color v1.18.0
 	github.com/golang-jwt/jwt/v5 v5.3.0
+	github.com/golang-migrate/migrate/v4 v4.18.3
 	github.com/google/uuid v1.6.0
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	golang.org/x/crypto v0.39.0
@@ -16,6 +17,8 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
@@ -23,10 +26,11 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/net v0.41.0 // indirect
 	golang.org/x/sync v0.15.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
--- auth-service/internal/domain/user_store.go
+++ auth-service/internal/domain/user_store.go
@@ -1,27 +1,27 @@
 package domain
 
-import "github.com/google/uuid"
+// import "github.com/google/uuid"
 
-// UserStore - привязка пользователя к магазину
-type UserStore struct {
-	UserID  uuid.UUID `gorm:"primaryKey;type:uuid"`
-	StoreID uuid.UUID `gorm:"primaryKey;type:uuid"` // id из CatalogService
+// // UserStore - привязка пользователя к магазину
+// type UserStore struct {
+// 	UserID  uuid.UUID `gorm:"primaryKey;type:uuid"`
+// 	StoreID uuid.UUID `gorm:"primaryKey;type:uuid"` // id из CatalogService
 
-	User User `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
-}
+// 	User User `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
+// }
 
-func (UserStore) TableName() string {
-	return "user_stores"
-}
+// func (UserStore) TableName() string {
+// 	return "user_stores"
+// }
 
-// UserCategory - привязка пользователя к категории
-type UserCategory struct {
-	UserID     uuid.UUID `gorm:"primaryKey;type:uuid"`
-	CategoryID uuid.UUID `gorm:"primaryKey;type:uuid"` // id из CatalogService
+// // UserCategory - привязка пользователя к категории
+// type UserCategory struct {
+// 	UserID     uuid.UUID `gorm:"primaryKey;type:uuid"`
+// 	CategoryID uuid.UUID `gorm:"primaryKey;type:uuid"` // id из CatalogService
 
-	User User `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
-}
+// 	User User `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
+// }
 
-func (UserCategory) TableName() string {
-	return "user_categories"
-}
+// func (UserCategory) TableName() string {
+// 	return "user_categories"
+// }
